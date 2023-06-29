93b341dce48fbe6848402ff9fd8806748c6c86fd
Johnny Solís <ingjsolisadv@gmail.com>
2023-06-29 12:14:41 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroModule } from './heroes/hero.module';
+import { CounterComponent } from './counter/components/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
